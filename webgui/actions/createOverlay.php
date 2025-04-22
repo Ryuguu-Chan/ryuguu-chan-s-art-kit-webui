1d34ef8e9e10eb3d09a81cea6c26ee24d3f2d7e8
Ogan Oezkul <oganozkul@gmail.com>
2025-04-22 17:04:47 +0200

    creation de la classe Rect

--- webgui/actions/createOverlay.php
+++ webgui/actions/createOverlay.php
@@ -1,64 +1,75 @@
 <?php
 
 
+// isset($_POST[nomDuBoutton])
+
 if (isset($_POST['canvasWidth']) && isset($_POST['canvasHeight'])) {
         
     header('Content-Type: image/png');
-    header('Content-Disposition: attachment; filename="RuleOfThird.png"');
 
     $width = $_POST['canvasWidth'];
     $height = $_POST['canvasHeight'];
 
-    $img = @imagecreatetruecolor($width, $height) or die ("cannot create the GD image stream");
+    if (isset($_POST['ruleOfThirdSubmission'])) {
+        header('Content-Disposition: attachment; filename="RuleOfThird.png"');
 
-    imagealphablending($img, false);
-    imagesavealpha($img, true);
+        $img = @imagecreatetruecolor($width, $height) or die ("cannot create the GD image stream");
 
-    $transparent = imagecolorallocatealpha($img, 0, 0, 0, 127);
-    imagefill($img, 0, 0, $transparent);
-    
-    $backgroundColor = imagecolorallocate($img, 0, 0, 0);
-    $lineColor = imagecolorallocate($img, 255, 0, 0);
-    
-    $imgWidthUnit = ($width/3);
-    $imgHeightUnit = ($height/3);
-    
-    // horizontal lines
-    imageline(
-        $img,
-        (int)$imgWidthUnit,
-        0,
-        (int)$imgWidthUnit,
-        $height,
-        $lineColor
-    );
-    imageline(
-        $img,
-        (int)$imgWidthUnit*2,
-        0,
-        (int)$imgWidthUnit*2,
-        $height,
-        $lineColor
-    );
-    
-    // vertical lines
-    imageline(
-        $img,
-        0,
-        (int)$imgHeightUnit,
-        $width,
-        (int)$imgHeightUnit,
-        $lineColor
-    );
-    imageline(
-        $img,
-        0,
-        (int)$imgHeightUnit*2,
-        $width,
-        (int)$imgHeightUnit*2,
-        $lineColor
-    );
-    
+        imagealphablending($img, false);
+        imagesavealpha($img, true);
+
+        $transparent = imagecolorallocatealpha($img, 0, 0, 0, 127);
+        imagefill($img, 0, 0, $transparent);
+        
+        $backgroundColor = imagecolorallocate($img, 0, 0, 0);
+        $lineColor = imagecolorallocate($img, 255, 0, 0);
+        
+        $imgWidthUnit = ($width/3);
+        $imgHeightUnit = ($height/3);
+        
+        // horizontal lines
+        imageline(
+            $img,
+            (int)$imgWidthUnit,
+            0,
+            (int)$imgWidthUnit,
+            $height,
+            $lineColor
+        );
+        imageline(
+            $img,
+            (int)$imgWidthUnit*2,
+            0,
+            (int)$imgWidthUnit*2,
+            $height,
+            $lineColor
+        );
+        
+        // vertical lines
+        imageline(
+            $img,
+            0,
+            (int)$imgHeightUnit,
+            $width,
+            (int)$imgHeightUnit,
+            $lineColor
+        );
+        imageline(
+            $img,
+            0,
+            (int)$imgHeightUnit*2,
+            $width,
+            (int)$imgHeightUnit*2,
+            $lineColor
+        );
+    }
+    else if ($_POST['goldenRatioSubmission']) {
+        header('Content-Disposition: attachment; filename="GoldenRatio.png"');
+
+        $img = @imagecreatetruecolor($width, $height) or die ("cannot create the GD image stream");
+        
+
+    }
     
     imagepng($img);
     imagedestroy($img);
---
+++ webgui/models/Rect.php
@@ -0,0 +1,11 @@
+<?php
+
+class Rect
+{
+    public function __construct($x1, $y1, $x2, $y2x1) {
+        $this->x1 = $x1;
+        $this->y1 = $y1;
+        $this->x2 = $x2;
+        $this->y2 = $y2;
+    }
+}
