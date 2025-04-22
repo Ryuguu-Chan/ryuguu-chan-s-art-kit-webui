681f87811eddce51cd7b4e90e66c512210c99b08
Ogan Oezkul <oganozkul@gmail.com>
2025-04-22 03:04:39 +0200

    creation du fichier strings.php

--- webgui/index.php
+++ webgui/index.php
@@ -1,1 +1,23 @@
-<?= "Hello World!" ?>
+<?php
+    include 'res/strings.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $strings["appTitle"]." (v".$strings["version"].")"; ?></title>
+</head>
+<body>
+    <header>
+        <h1><?= $strings["appTitle"]." (".$strings["appAuthor"].") (v".$strings["version"].")";?></h1>
+    </header>
+    <div class="menuDiv">
+        <!-- TODO: faire le menu ici -->
+    </div>
+    <div class="content">
+
+    </div>
+</body>
+</html>
---
+++ webgui/res/strings.php
@@ -0,0 +1,7 @@
+<?php
+
+$strings = array(
+    "appTitle" => "Ryuguu Chan's art kit (webui)",
+    "appAuthor" => "made by Ogan Özkul aka Ryuguu Chan",
+    "version" => "1.0"
+);
