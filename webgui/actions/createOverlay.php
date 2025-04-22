971ef89939c52f485f2b60922fb6e07577a884cb
Ogan Oezkul <oganozkul@gmail.com>
2025-04-22 18:01:00 +0200

    creation de la methode partition

--- webgui/actions/createOverlay.php
+++ webgui/actions/createOverlay.php
@@ -1,7 +1,18 @@
 <?php
 
+// a/b = (a+b)/a = 1.618 (phi).
+function partition($length) {
 
-// isset($_POST[nomDuBoutton])
+    // nani?
+    $PHI = 1.618;
+    $b = $length / ($PHI+1);
+    $a = $PHI + $b;
+
+    return [
+        "a" => $a,
+        "b" => $b
+    ];
+}
 
 if (isset($_POST['canvasWidth']) && isset($_POST['canvasHeight'])) {
         
@@ -10,13 +21,13 @@ if (isset($_POST['canvasWidth']) && isset($_POST['canvasHeight'])) {
     $width = $_POST['canvasWidth'];
     $height = $_POST['canvasHeight'];
 
+    $img = @imagecreatetruecolor($width, $height) or die ("cannot create the GD image stream");
+
+    imagealphablending($img, false);
+    imagesavealpha($img, true);
+
     if (isset($_POST['ruleOfThirdSubmission'])) {
         header('Content-Disposition: attachment; filename="RuleOfThird.png"');
-
-        $img = @imagecreatetruecolor($width, $height) or die ("cannot create the GD image stream");
-
-        imagealphablending($img, false);
-        imagesavealpha($img, true);
 
         $transparent = imagecolorallocatealpha($img, 0, 0, 0, 127);
         imagefill($img, 0, 0, $transparent);
@@ -64,10 +75,17 @@ if (isset($_POST['canvasWidth']) && isset($_POST['canvasHeight'])) {
         );
     }
     else if ($_POST['goldenRatioSubmission']) {
+        
+        // source: https://blog.ansi.org/the-golden-ratio-standard-art-nature-space-time/?gad_source=1&gclid=Cj0KCQjw_JzABhC2ARIsAPe3ynp3xxqzxqiVxj7NVMA1R0u96wtmNhk0udkPAZ7B5K3NsshFzC3iLtEaAt2ZEALw_wcB
+
+        include '../models/Rect.php';
         header('Content-Disposition: attachment; filename="GoldenRatio.png"');
 
         $img = @imagecreatetruecolor($width, $height) or die ("cannot create the GD image stream");
-        
+
+        for ($i = 0; $i < 8; $i++) {
+            
+        }
 
     }
     
