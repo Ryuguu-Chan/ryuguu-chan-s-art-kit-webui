7a52563f324045da1fba6e62d33a76cba51465fb
Ogan Oezkul <oganozkul@gmail.com>
2025-04-22 15:57:39 +0200

    création du fichier ruleOfThird.php

---
+++ webgui/actions/ruleOfThird.php
@@ -0,0 +1,67 @@
+<?php
+
+
+if (isset($_POST['canvasWidth']) && isset($_POST['canvasHeight'])) {
+        
+    header('Content-Type: image/png');
+
+    $width = $_POST['canvasWidth'];
+    $height = $_POST['canvasHeight'];
+
+    $img = @imagecreatetruecolor($width, $height) or die ("cannot create the GD image stream");
+
+    imagealphablending($img, false);
+    imagesavealpha($img, true);
+
+    $transparent = imagecolorallocatealpha($img, 0, 0, 0, 127);
+    imagefill($img, 0, 0, $transparent);
+    
+    $backgroundColor = imagecolorallocate($img, 0, 0, 0);
+    $lineColor = imagecolorallocate($img, 255, 0, 0);
+    
+    $imgWidthUnit = ($width/3);
+    $imgHeightUnit = ($height/3);
+    
+    // horizontal lines
+    imageline(
+        $img,
+        (int)$imgWidthUnit,
+        0,
+        (int)$imgWidthUnit,
+        $height,
+        $lineColor
+    );
+    imageline(
+        $img,
+        (int)$imgWidthUnit*2,
+        0,
+        (int)$imgWidthUnit*2,
+        $height,
+        $lineColor
+    );
+    
+    // vertical lines
+    imageline(
+        $img,
+        0,
+        (int)$imgHeightUnit,
+        $width,
+        (int)$imgHeightUnit,
+        $lineColor
+    );
+    imageline(
+        $img,
+        0,
+        (int)$imgHeightUnit*2,
+        $width,
+        (int)$imgHeightUnit*2,
+        $lineColor
+    );
+    
+    
+    imagepng($img);
+    imagedestroy($img);
+}
+else {
+    die("cannot process the image");
+}
