a07f2db7faee22a1276975c66d98869258786d58
Ogan Oezkul <oganozkul@gmail.com>
2025-04-22 21:49:43 +0200

    première stylisation de la page

--- webgui/index.php
+++ webgui/index.php
@@ -7,21 +7,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/main.css" media="all">
     <title><?= $strings["appTitle"]." (v".$strings["version"].")"; ?></title>
 </head>
 <body>
     <header>
         <h1><?= $strings["appTitle"]." (".$strings["appAuthor"].") (v".$strings["version"].")";?></h1>
     </header>
-    <div class="menuDiv">
-        <!-- TODO: faire le menu ici -->
-    </div>
     <div class="content">
         <form action="actions/createOverlay.php" method="post">
             <h2>Canvas's width</h2>
-            <input type="number" name="canvasWidth" id="canvasWidthTextBox">
+            <input type="number" name="canvasWidth" id="canvasWidthTextBox" class="inputField">
             <h2>Canvas's height</h2>
-            <input type="number" name="canvasHeight" id="canvasHeightTextBox">
+            <input type="number" name="canvasHeight" id="canvasHeightTextBox" class="inputField">
             <input type="submit" name="ruleOfThirdSubmission" value="Generate a rule of third overlay" class="submissionButton" disabled>
             <input type="submit" name="goldenRatioSubmission" value="Generate a golden ratio overlay" class="submissionButton" disabled>
         </form>
